92a57461c795251cac970872906cdc5c671565a2
Alejandro Mejia <alejomejiacuartas@outlook.com>
2026-01-20 16:40:35 +0100

    remove unused files causing issues, add them later on

--- src/app/[page]/opengraph-image.tsx
+++
@@ -1,9 +0,0 @@
-import OpengraphImage from '@/components/from-template/opengraph-image'
-import { getPage } from '@/lib/integrations/shopify'
-
-export default async function Image({ params }: { params: { page: string } }) {
-  const page = await getPage(params.page)
-  const title = page.seo?.title || page.title
-
-  return await OpengraphImage({ title })
-}
--- src/app/search/[collection]/opengraph-image.tsx
+++
@@ -1,9 +0,0 @@
-import OpengraphImage from '@/components/from-template/opengraph-image'
-import { getCollection } from '@/lib/integrations/shopify'
-
-export default async function Image({ params }: { params: { collection: string } }) {
-  const collection = await getCollection(params.collection)
-  const title = collection?.seo?.title || collection?.title
-
-  return await OpengraphImage({ title })
-}
