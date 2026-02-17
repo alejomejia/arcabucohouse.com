a4bd2f8f5ea3c74a7355bed65a92388f274420c4
Alejandro Mejia <alejomejiacuartas@outlook.com>
2026-02-17 11:26:43 +0100

    add selection styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,7 +42,7 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
-      <body className={cn("font-sans antialiased text-primary-base", sans.variable, serif.variable)}>
+      <body className={cn("font-sans antialiased text-primary-base selection:bg-primary-base selection:text-primary-100", sans.variable, serif.variable)}>
         <PortalRoot id={PORTAL_IDS.bodyTop} />
         <Toaster closeButton />
 
