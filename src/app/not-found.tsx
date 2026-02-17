cc18839ddf87243f332ee5bb9ad437dfea0c3094
Alejandro Mejia <alejomejiacuartas@outlook.com>
2026-02-17 11:02:11 +0100

    update not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,32 +1,22 @@
-import Link from 'next/link'
+import { UnderlineLink } from "@/components/effects/underline/underline-link";
 
-import { cn } from '@/lib/utils/helpers'
 
 export default function NotFound() {
   return (
-    <div
-      className={cn(
-        'mx-auto my-4 flex max-w-xl flex-col items-center',
-        'rounded-lg border border-neutral-200 bg-white p-8',
-        'md:p-12',
-        'dark:border-neutral-800 dark:bg-black'
-      )}
-    >
-      <h2 className="text-xl font-bold">Page Not Found</h2>
-      <p className="my-2 text-center">
-        The page you are looking for does not exist or may have been moved.
-      </p>
-      <Link
-        href="/"
-        className={cn(
-          'mx-auto mt-4 flex w-full items-center justify-center',
-          'rounded-full bg-blue-600 p-4',
-          'tracking-wide text-white',
-          'hover:opacity-90'
-        )}
-      >
-        Go Back Home
-      </Link>
-    </div>
+    <section className="px-6 pt-16 min-h-screen flex flex-col justify-center text-primary-base">
+      <div className="pb-4 border-b border-primary-100">
+        <h1 className="text-xl uppercase tracking-wider font-semibold text-primary-300">Not Found</h1>
+      </div>
+      <div className="pt-8 flex flex-col lg:flex-row justify-between gap-10 mb-12 font-serif">
+        <div className="w-full flex gap-4 md:gap-6 text-6xl md:text-9xl italic leading-none">
+          <span className="lg:flex-1">Error</span>
+          <span className="lg:flex-1">404</span>
+        </div>
+        <p className="max-w-sm text-lg text-balance">The page you are looking for does not exist or may have been moved.</p>
+      </div>
+      <UnderlineLink className="text-xl uppercase font-serif" href="/">
+        Return to Home
+      </UnderlineLink>
+    </section>
   )
 }
